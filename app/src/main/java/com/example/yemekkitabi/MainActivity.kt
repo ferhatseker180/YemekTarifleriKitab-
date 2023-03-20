342c05c7faa9d206035a16d86556847cb6022a93
ferhatseker180 <ferhatseker180@gmail.com>
2023-03-21 00:58:01 +0300

    Yemek Tarifleri Uygulaması Temel Fonksiyonlar,Bitmap, İzin Kontrolleri Gerçekleştirildi.

---
+++ app/src/main/java/com/example/yemekkitabi/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.yemekkitabi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.navigation.Navigation
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        val menuInflater = getMenuInflater().inflate(R.menu.yemek_ekle,menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.action_ekle) {
+            val action = ListeFragmentDirections.actionListeFragmentToTarifFragment()
+            Navigation.findNavController(this,R.id.fragment).navigate(action)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
---
+++ app/src/main/java/com/example/yemekkitabi/TarifFragment.kt
@@ -0,0 +1,156 @@
+package com.example.yemekkitabi
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_tarif.*
+import java.io.ByteArrayOutputStream
+import java.security.Permission
+
+class TarifFragment : Fragment() {
+    var secilenGorsel : Uri? = null
+    var secilenBitmap : Bitmap? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_tarif, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        buttonKaydet.setOnClickListener {
+            kaydet(it)
+        }
+
+        imageView.setOnClickListener {
+
+            gorselEkle(it)
+        }
+
+    }
+
+    fun kaydet(view: View) {
+// SQlite'a veri kaydetme
+
+        val yemekIsmi = editTextYemekIsmi.text.toString()
+        val yemekMalzeme = editTextYemekMalzeme.text.toString()
+
+        if (secilenBitmap != null) {
+
+            val kucukBitmap = kucukBitmapOlustur(secilenBitmap!!,300)
+            val outputStream = ByteArrayOutputStream()
+            kucukBitmap.compress(Bitmap.CompressFormat.JPEG,50,outputStream)
+            val byteDizisi = outputStream.toByteArray()
+
+            try {
+
+                context?.let {
+                    val database = it!!.openOrCreateDatabase("Yemekler",Context.MODE_PRIVATE,null)
+                    database.execSQL("CREATE TABLE IF NOT EXISTS yemekler(id INTEGER PRIMARY KEY, yemekismi VARCHAR, yemekmalzemesi VARCHAR, gorsel BLOB)")
+                    val sqlString = "INSERT INTO yemekler(yemekismi, yemekmalzemesi,gorsel) VALUES (?,?,?)"
+                    val statement = database.compileStatement(sqlString)
+                    statement.bindString(1,yemekIsmi)
+                    statement.bindString(2,yemekMalzeme)
+                    statement.bindBlob(3,byteDizisi)
+                    statement.execute()
+                }
+
+            } catch (e : Exception) {
+                e.printStackTrace()
+            }
+            val action = TarifFragmentDirections.actionTarifFragmentToListeFragment()
+            Navigation.findNavController(view).navigate(action)
+        }
+
+    }
+
+    fun gorselEkle(view: View) {
+
+   activity?.let {
+       if (ContextCompat.checkSelfPermission(it.applicationContext,android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+
+           requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),1)
+       } else {
+
+           val galeriIntent = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+           startActivityForResult(galeriIntent,2)
+       }
+
+   }
+
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+
+        if (requestCode == 1) {
+
+            if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                val galeriIntent = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(galeriIntent,2)
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        if (requestCode == 2 && resultCode == Activity.RESULT_OK && data != null) {
+
+            secilenGorsel = data.data
+
+            try {
+                context?.let {
+                    if (secilenGorsel != null) {
+                        val source = ImageDecoder.createSource(it.contentResolver,secilenGorsel!!)
+                        secilenBitmap = ImageDecoder.decodeBitmap(source)
+                        imageView.setImageBitmap(secilenBitmap)
+                    }
+                }
+
+            }catch (e : Exception) {
+
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    fun kucukBitmapOlustur(kullanicininSectigiBitmap : Bitmap, maximumBoyut : Int) : Bitmap {
+
+        var width = kullanicininSectigiBitmap.width
+        var height = kullanicininSectigiBitmap.height
+
+        val bitmapOrani : Double = width.toDouble() / height.toDouble()
+
+        if (bitmapOrani > 1) {
+
+            width = maximumBoyut
+            val kisaltilmisHeight = width / bitmapOrani
+            height = kisaltilmisHeight.toInt()
+
+        } else {
+            height = maximumBoyut
+            val kisaltilmisWidth = height / bitmapOrani
+           width = kisaltilmisWidth.toInt()
+        }
+
+        return Bitmap.createScaledBitmap(kullanicininSectigiBitmap,width,height,true)
+    }
+
+
+}
